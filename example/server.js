1d0eaf31d8c762a289f8791337774955913d3fd4
Forbes Lindesay <forbes@lindesay.co.uk>
2016-07-16 16:14:48 +0100

    Add missing port number

--- example/server.js
+++ example/server.js
@@ -8,4 +8,4 @@ app.get('/', function (req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
-module.exports = app.listen(3000);
+module.exports = app.listen(process.env.PORT || 3000);
